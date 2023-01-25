ee0be4f6a4fb2ef9e5bff9aca71baf7f20ec1c5d
khadyjh11 <khadyjh-naif@outlook.com>
2023-01-25 11:40:42 +0300

    first

---
+++ udagram/set_env.sh
@@ -0,0 +1,15 @@
+# This filLe is used for convenience of local development..
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.crlvjtlgkcwn.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::khadyjh-bucket
+export PORT = 8080
+export DB_PORT=5432
+export AWS_REGION=us-east-1
+export AWS_PROFILE=defualt
+export JWT_SECRET=mysecretstring
+export URL=udagram-api-dev.eba-yz3jsneq.us-east-1.elasticbeanstalk.com
+export AWS_ACCESS_KEY_ID = "AKIA6NMS6YBUKNF3TN6A"
+export AWS_SECRET_ACCESS_KEY = "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"
